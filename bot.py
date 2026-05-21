8371f3821e4f7654659928748c7206aa60243a03
elTokyo <103259437+elTokyo@users.noreply.github.com>
2026-05-21 17:33:53 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -73,6 +73,7 @@ def main():
     app.add_handler(CommandHandler("debug",     handlers.cmd_debug))
     app.add_handler(CommandHandler("checkfonbet", handlers.cmd_checkfonbet))
     app.add_handler(CommandHandler("syncdiscord", handlers.cmd_syncdiscord))
+    app.add_handler(CommandHandler("factors",     handlers.cmd_factors))
     app.add_handler(CommandHandler("broadcast", handlers.cmd_broadcast))
     app.add_handler(CommandHandler("cancel",    handlers.cmd_cancel))
 
